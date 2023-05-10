e7334b1ed60c1dab56cb78621fe263a292c3dff7
emanuelefavero <emafaux89@gmail.com>
2023-05-11 01:31:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <HelloWorld msg="Ciao" />
+  </div>
+</template>
+
+<style scoped>
+/* ... */
+.container {
+  color: magenta;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(1)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <h2>{{ count }}</h2>
+  <button
+    type="button"
+    @click="
+      () => {
+        if (count > 0) {
+          count--
+        }
+      }
+    "
+  >
+    -
+  </button>
+  <button type="button" @click="() => count++">+</button>
+</template>
