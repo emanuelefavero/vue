de2c78728f454331ea4cc6a1790ffe5e0aeb1392
emanuelefavero <emafaux89@gmail.com>
2023-05-12 23:03:33 +0200

    add Todo code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Hello from './components/Hello.vue'
+import Todo from './components/Todo.vue'
 </script>
 
 <template>
   <Hello greet="Ciao" />
+  <Todo />
 </template>
---
+++ src/components/Todo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const todos = ref<string[]>(['sleep', 'eat', 'code'])
+const newTodo = ref('')
+</script>
+
+<template>
+  <h1>Todo</h1>
+
+  <input type="text" v-model="newTodo" />
+  <button type="button" @click="() => todos.push(newTodo)">Add</button>
+
+  <p>
+    {{ todos.length === 0 ? 'No todos yet' : '' }}
+  </p>
+
+  <ol>
+    <li v-for="(todo, index) in todos">
+      {{ todo }}
+      <button type="button" @click="() => todos.splice(index, 1)">x</button>
+    </li>
+  </ol>
+</template>
